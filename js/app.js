eeffed71f7e0d3f04afe84699fe5bf3684282978
Alex G <58758478+farmfram@users.noreply.github.com>
2024-12-04 14:09:46 -0800

    started triangle calculator

--- js/app.js
+++ js/app.js
@@ -79,7 +79,7 @@ function rightAngleCalculator() {
         if (sideA !== 0 && sideB !== 0) {
             angleA = Math.atan2(sideA, sideB) * (180 / Math.PI)
             angleB = (90 - angleA)
-            sideC = Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2))
+            sideC = Math.sqrt((sideA**2) + (sideB**2))
         }
         if (sideA !== 0 && sideC !== 0) {
             if (sideC < sideA) {
@@ -89,7 +89,7 @@ function rightAngleCalculator() {
             } else {
                 angleA = Math.asin(sideA / sideC) / (Math.PI / 180)
                 angleB = (90 - angleA)
-                sideB = Math.sqrt(Math.pow(sideC, 2) - Math.pow(sideA, 2))
+                sideB = Math.sqrt((sideC**2) - (sideA**2))
             }
         }
         if (sideC !== 0 && sideB !== 0) {
@@ -100,7 +100,7 @@ function rightAngleCalculator() {
             } else {
                 angleA = Math.acos(sideB / sideC) / (Math.PI / 180)
                 angleB = (90 - angleA)
-                sideA = Math.sqrt(Math.pow(sideC, 2) - Math.pow(sideB, 2))
+                sideA = Math.sqrt((sideC**2) - (sideB**2))
             }
         }
 
@@ -144,94 +144,50 @@ function triangleCalculator() {
 
     //check number of inputs recieved
     let inputCount = 0
+    let angleCount = 0
+    let sideCount = 0
     if (angleA !== 0) {
+        angleCount++
         inputCount++
     }
     if (angleB !== 0) {
+        angleCount++
         inputCount++
     }
     if (angleC !== 0) {
+        angleCount++
         inputCount++
     }
     if (sideA !== 0) {
+        sideCount++
         inputCount++
     }
     if (sideB !== 0) {
+        sideCount++
         inputCount++
     }
     if (sideC !== 0) {
+        sideCount++
         inputCount++
     }
     // perform calculation
-    if (inputCount === 3 && (angleA === 0 || angleB === 0) && (angleA < 90 && angleB < 90) && (angleA >= 0 && angleB >= 0)) { //this section needs re-work for this calculator and is currently commented out, 
-        //turn into two different if sections for cos and sin
-        //create a cosine law section and a sin law section^
+    if (inputCount === 3 && ((sideCount === 2 && angleCount === 1) || (sideCount === 1 && angleCount === 2))){ //check that a valid number of inputs are recieved before deciding what operation to perform
 
-        // angleA options
-        if (angleA !== 0 && sideA !== 0) {
-            angleB = (90 - angleA)
-            sideB = sideA / Math.tan(angleA * (Math.PI / 180))
-            sideC = sideA / Math.sin(angleA * (Math.PI / 180))
-        }
-        if (angleA !== 0 && sideB !== 0) {
-            angleB = (90 - angleA)
-            sideA = sideB * Math.tan(angleA * (Math.PI / 180))
-            sideC = sideB / Math.cos(angleA * (Math.PI / 180))
-        }
-        if (angleA !== 0 && sideC !== 0) {
-            angleB = (90 - angleA)
-            sideA = sideC * Math.sin(angleA * (Math.PI / 180))
-            sideB = sideC * Math.cos(angleA * (Math.PI / 180))
-        }
+        if((angleC !== 0 && sideA !== 0 && sideB !== 0)){
+            sideC = Math.sqrt((sideA**2)+(sideB**2)-(2*sideA*sideB*Math.cos(angleC * (Math.PI / 180))))
 
-        //angleB options
-        if (angleB !== 0 && sideA !== 0) {
-            angleA = (90 - angleB)
-            sideB = sideA * Math.tan(angleB * (Math.PI / 180))
-            sideC = sideA / Math.cos(angleB * (Math.PI / 180))
-        }
-        if (angleB !== 0 && sideB !== 0) {
-            angleA = (90 - angleB)
-            sideA = sideB / Math.tan(angleB * (Math.PI / 180))
-            sideC = sideB / Math.sin(angleB * (Math.PI / 180))
-        }
-        if (angleB !== 0 && sideC !== 0) {
-            angleA = (90 - angleB)
-            sideA = sideC * Math.cos(angleB * (Math.PI / 180))
-            sideB = sideC * Math.sin(angleB * (Math.PI / 180))
-        }
+        }else if(angleB !== 0 && sideA !== 0 && sideC !== 0){
+            sideB = Math.sqrt((sideA**2)+(sideC**2)-(2*sideA*sideC*Math.cos(angleB * (Math.PI / 180))))
 
-        // Side options
-        if (sideA !== 0 && sideB !== 0) {
-            angleA = Math.atan2(sideA, sideB) * (180 / Math.PI)
-            angleB = (90 - angleA)
-            sideC = Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2))
-        }
-        if (sideA !== 0 && sideC !== 0) {
-            if (sideC < sideA) {
-                alert("Side C must be larger than Side A")  // Alert for invalid inputs
-                alertState = 1
-                resetPage()
-            } else {
-                angleA = Math.asin(sideA / sideC) / (Math.PI / 180)
-                angleB = (90 - angleA)
-                sideB = Math.sqrt(Math.pow(sideC, 2) - Math.pow(sideA, 2))
-            }
-        }
-        if (sideC !== 0 && sideB !== 0) {
-            if (sideC < sideB) {
-                alert("Side C must be larger than Side B")  // Alert for invalid inputs
-                alertState = 1
-                resetPage()
-            } else {
-                angleA = Math.acos(sideB / sideC) / (Math.PI / 180)
-                angleB = (90 - angleA)
-                sideA = Math.sqrt(Math.pow(sideC, 2) - Math.pow(sideB, 2))
-            }
+        }else if(angleA !== 0 && sideC !== 0 && sideB !== 0){
+            sideA = Math.sqrt((sideC**2)+(sideB**2)-(2*sideC*sideB*Math.cos(angleA * (Math.PI / 180))))
+
+        }else{ //Use Sine Law
+
         }
 
     } else {
-        alert("must enter either two sides or a side and an angle")   // Alert for invalid inputs
+        alert("WRONG!")   // Alert for invalid inputs
         alertState = 1
         resetPage()
     }
@@ -253,8 +209,8 @@ function triangleCalculator() {
 let rightAngleButton = document.getElementById('rightAngleCalculate')
 rightAngleButton.addEventListener('click', rightAngleCalculator)
 
-// let triangleButton = document.getElementById('triangleCalculate')
-// triangleButton.addEventListener('click', triangleCalculator)
+let triangleButton = document.getElementById('triangleCalculate')
+triangleButton.addEventListener('click', triangleCalculator)
 
 // reset-buttons
 let buttonVar1 = document.getElementById('right-angle-reset')
